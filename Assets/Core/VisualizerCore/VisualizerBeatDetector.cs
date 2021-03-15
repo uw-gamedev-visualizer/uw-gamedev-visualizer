193ebcef1a4765626621b2c35f7f7711fc4fb805
RShields <playnow254@gmail.com>
2021-03-15 15:57:00 -0700

    Implement Beat Detection
    v1

--- Assets/Core/VisualizerCore/VisualizerBeatDetector.cs
+++ Assets/Core/VisualizerCore/VisualizerBeatDetector.cs
@@ -7,80 +7,134 @@ namespace Visualizer
 {
     public static class VisualizerBeatDetector
     {
-        public static float BufferTime = 0.5f;
-        public static float MinBeatMultiplier = 2;
-        public static float NoiseGate = 0.02f;
+        public const float BufferTime = 2.2f;
+        public const float NoiseGate = 0.6f;
+        public const float MinBpm = 99;
+        public const float MaxBpm = 183;
 
-        private class SpectrumData
+        public static float LastBeat { get; private set; }
+        public static float SecsPerBeat { get; private set; }
+        public static float BPM => 60 / SecsPerBeat;
+        public static float SecsToBeat => LastBeat + SecsPerBeat - Time.unscaledTime;
+        public static float BeatFraction => 1 - SecsToBeat / SecsPerBeat;
+        public static bool IsBeat { get; private set; }
+
+        private static bool _bufferFull;
+
+        private struct BufferData
         {
-            public float time;
-            public float max;
+            public float Time;
+            public float Level;
         }
-        
-        private static Queue<SpectrumData> _prevSpectra = new Queue<SpectrumData>();
 
-        private static float _level;
-        private static bool _beatOld = true;
-        private static bool _beat;
+        private static Queue<BufferData> _buffer = new Queue<BufferData>();
 
         public static void Update()
         {
-            _beatOld = true;
-            _level = VisualizerCore.Level(FilterType.LowPass);
-            UpdatePrevs();
-        }
-
-        public static bool IsBeat()
-        {
-            if (!_beatOld)
+            IsBeat = false;
+            float level = Mathf.Max(VisualizerCore.Level(FilterType.LowPass), 0);
+            UpdateBuffer(level);
+            
+            if (_bufferFull)
             {
-                return _beat;
+                float oldLastBeat = LastBeat;
+                CheckBeat();
+                if (oldLastBeat != LastBeat)
+                {
+                    IsBeat = true;
+                }
             }
 
-            if (_prevSpectra.Count < 1)
-            {
-                return false;
-            }
-
-            float prevAvg = _prevSpectra.Sum(x => x.max) / _prevSpectra.Count;
-            _beat = _level > NoiseGate && _level > prevAvg * MinBeatMultiplier;
-            _beatOld = false;
-            return _beat;
+            DebugVisualize(_buffer.ToList(), Color.cyan);
         }
 
-        /*
-        public struct BeatDetectorData
+        private static void UpdateBuffer(float level)
         {
-            public float level;
-            public float prevAvg;
-            public float minBeatMultiplier;
-            public float noiseGate;
-        }
-
-        [System.Obsolete("Debug only!", false)]
-        public static BeatDetectorData GetData()
-        {
-            return new BeatDetectorData
+            _buffer.Enqueue(new BufferData
             {
-                level = _level,
-                noiseGate = NoiseGate,
-                minBeatMultiplier = MinBeatMultiplier,
-                prevAvg = _prevSpectra.Sum(x => x.max) / _prevSpectra.Count
-            };
-        }
-        */
-
-        private static void UpdatePrevs()
-        {
-            _prevSpectra.Enqueue(new SpectrumData
-            {
-                time = Time.unscaledTime,
-                max = _level
+                Time = Time.unscaledTime,
+                Level = level
             });
 
-            while (_prevSpectra.Peek().time < Time.unscaledTime - BufferTime)
+            _bufferFull = false;
+            while (_buffer.Peek().Time < Time.unscaledTime - BufferTime)
             {
-                _prevSpectra.Dequeue();
+                _bufferFull = true;
+                _buffer.Dequeue();
+            }
+        }
+
+        private static void CheckBeat()
+        {
+            BufferData[] bufferList = _buffer.ToArray();
+            //float avg = bufferList.Select(x => x.Level).Average();
+            List<BufferData> jumps = new List<BufferData>();
+            for (int i = 1; i < bufferList.Length; i++)
+            {
+                float avg = 0;
+                float max = 0;
+                for (int j = Mathf.Max(i - 10, 0); j < i; j++)
+                {
+                    avg += bufferList[j].Level;
+                    max = Mathf.Max(max, bufferList[j].Level);
+                }
+                avg /= 10;
+                
+                if (avg < 0.5f && max < 0.8f && bufferList[i].Level > 0.8f)
+                {
+                    jumps.Add(bufferList[i]);
+                }
+            }
+            DebugVisualize(jumps, Color.red);
+
+            if (jumps.Count < 2)
+            {
+                return;
+            }
+
+            List<float> gaps = new List<float>();
+            for (int i = 1; i < jumps.Count; i++)
+            {
+                gaps.Add(jumps[i].Time - jumps[i - 1].Time);
+            }
+
+            SecsPerBeat = gaps[gaps.Count - 1]; //TODO
+            FixSecsPerBeat();
+            LastBeat = jumps[jumps.Count - 1].Time;
+        }
+
+        private static void FixSecsPerBeat()
+        {
+            float minSecs = 60 / MaxBpm;
+            for (int i = 0; i < 3 && SecsPerBeat < minSecs; i++)
+            {
+                SecsPerBeat *= 2;
+            }
+            float maxSecs = 60 / MinBpm;
+            for (int i = 0; i < 3 && SecsPerBeat > maxSecs; i++)
+            {
+                SecsPerBeat /= 2;
+            }
+        }
+
+        private static void DebugVisualize(List<BufferData> list, Color color)
+        {
+            if (list.Count < 1)
+            {
+                return;
+            }
+
+            BufferData prev = new BufferData {Time = -1};
+            foreach (BufferData bd in list)
+            {
+                if (prev.Time >= 0)
+                {
+                    Vector3 prevPos = new Vector3(-(Time.unscaledTime - prev.Time), prev.Level);
+                    Vector3 nextPos = new Vector3(-(Time.unscaledTime - bd.Time), bd.Level);
+                    Debug.DrawLine(prevPos, nextPos, color);
+                }
+
+                prev = bd;
             }
         }
     }
